b4c502404cababe1f69cc9287f84bd7a6cb94409
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2022-06-04 17:48:58 +0200

    Updated styling to implement a (very) basic design

--- djangoforms/djangoforms/settings.py
+++ djangoforms/djangoforms/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+
+# Load static files in debug mode
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
